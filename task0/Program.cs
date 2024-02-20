2003768592b7de25480ff3fc6079feb7a60dad88
Sergei Zubov <tehwas@ya.ru>
2024-02-21 01:36:34 +0300

    this0

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number*number;
+Console.WriteLine("Квадрат числа = " +result);
